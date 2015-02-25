04410725596861eb3a70db973c649ddaa1c57878
JChauster <chaudown5@hotmail.com>
2015-02-25 14:41:08 -0500

    Revert "Emailer skeleton (from Sarah's project)"
    This reverts commit 1ee1abadfd25399fbdd4ce38f5f38d17039002b8.

--- app/routes/email.server.routes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(app) {
-	// Routing logic   
-	var email = require('../../app/controllers/email.server.controller.js');
-
-	// Setting up the medicalHistory api
-	app.route('/email/notifyCustomer').post(email.emailNotification);
-};
--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -7,11 +7,11 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 		$scope.menu = Menus.getMenu('topbar');
                 $scope.user = Authentication.user;
                 
-                if($scope.user.userType === 'Current')
+                if($scope.user.userType == 'Current')
                     $scope.type = 'current';
-                else if($scope.user.userType === 'Prospective')
+                else if($scope.user.userType == 'Prospective')
                     $scope.type = 'prospective';
-                else if($scope.user.userType === 'Alumni')
+                else if($scope.user.userType == 'Alumni')
                     $scope.type = 'alumni';
                 else
                     $scope.type = '';
@@ -24,5 +24,15 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 			$scope.isCollapsed = false;
 		});
                 
+                $scope.goHome = function (){
+                    if($scope.user.userType==="Current")
+                        $location.path('/home/current');
+                    else if($scope.user.userType==="Prospective")
+                         $location.path('/home/prospective');
+                    else if($scope.user.userType==="Alumni")
+                        $location.path('/home/alumni');
+                    else
+                        $location.path('/home');
+                };
 	}
 ]);
