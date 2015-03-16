2ce564f42a4517f939eb49499bd44febdccb4a3f
Benjamin Meredith <bmeredith@ufl.edu>
2015-03-16 14:27:19 -0400

    Fixed issue with alumni signup
    Alumni could not enter their graduation year; now they can

---
+++ public/modules/core/controllers/uniform-repairs.client.controller.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('core').controller('UniformRepairsController', ['$scope', '$http', 'Authentication',
+	function($scope, $http, Authentication) {
+		// This provides Authentication context.
+		$scope.authentication = Authentication;
+                
+                $scope.sendEmail = function (){
+
+                    $http.post('/email/repairEmail').success(function(response) {
+                    }).error(function(response) {
+				$scope.error = response.message;
+			});
+                };
+	}
+]);
