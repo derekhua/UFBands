e9226b47aca4b058737bc924e156634f591d3b66
JChauster <chaudown5@hotmail.com>
2015-03-17 16:31:35 -0400

    page redirect after alert

--- public/modules/core/controllers/instrument-repairs.client.controller.js
+++ public/modules/core/controllers/instrument-repairs.client.controller.js
@@ -1,9 +1,10 @@
 'use strict';
 
-angular.module('core').controller('InstrumentRepairsController', ['$scope', '$http', 'Authentication',
-	function($scope, $http, Authentication) {
+angular.module('core').controller('InstrumentRepairsController', ['$scope', '$http', '$location', 'Authentication',
+	function($scope, $http, $location, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+                $scope.user = Authentication.user;
                 
                 $scope.sendEmail = function (){
 
@@ -15,7 +16,13 @@ angular.module('core').controller('InstrumentRepairsController', ['$scope', '$ht
                     	console.log(response);
 				$scope.error = response.message;
 			});
-                alert("Thank you for submitting your repair request.");
+                alert("Thank you for submitting your repair request. \nPlease press OK to finalize");
+                if($scope.user.userType==='Current')
+                        $location.path('/home/current');
+                    else if($scope.user.userType==='Prospective')
+                         $location.path('/home/prospective');
+                    else if($scope.user.userType==='Alumni')
+                        $location.path('/home/alumni');
                 };
 	}
 ]);
