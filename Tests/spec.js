7a289765a2fc4b71988f498d4d6569f60b49534e
Gio De La Torre <g.delatorre@aol.com>
2015-03-20 15:25:47 -0400

    Adjusted Tests
    Adjusted tests to match new sign up structure

--- Tests/spec.js
+++ Tests/spec.js
@@ -44,14 +44,31 @@ describe('CEN3031 login', function() {
 
         element(by.id('firstName')).sendKeys('Test');
         element(by.id('lastName')).sendKeys('Test');
+        element(by.id('nickName')).sendKeys('Test');
         element(by.id('email')).sendKeys('test@test.com');
+        element(by.id('gatorlink')).sendKeys('test@ufl.edu');
         element(by.id('buttonProspective')).click();
+        //element(by.id('instrument')).sendKeys('Trombone');
+        element(by.id('primary')).click();
+        element.all(by.repeater('instruments in instruments')).click();
+        element(by.id('primaryYears')).sendKeys('Test');
+        element(by.id('phoneNumber')).sendKeys('Test');
+        element(by.id('highSchool')).sendKeys('Test');
         element(by.id('username')).sendKeys(newUserNameProspective);
         element(by.id('password')).sendKeys('TestTest');
-        element(by.id('instrument')).sendKeys('Trombone');
-        element(by.id('highSchool')).sendKeys('Test');
-        element(by.id('phoneNumber')).sendKeys('Test');
-        element(by.id('permanentAddress')).sendKeys('Test');
+        element(by.id('permanentLine1')).sendKeys('Test');
+        element(by.id('permanentLine2')).sendKeys('Test');
+        element(by.id('permanentCity')).sendKeys('Test');
+        element(by.id('permanentState')).sendKeys('Fl');
+        element(by.id('permanentZip')).sendKeys('32611');
+        element(by.id('localLine1')).sendKeys('Test');
+        element(by.id('localLine2')).sendKeys('Test');
+        element(by.id('localCity')).sendKeys('Test');
+        element(by.id('localState')).sendKeys('Fl');
+        element(by.id('localZip')).sendKeys('32611');
+        element(by.id('major')).sendKeys('Test');
+        element(by.id('class')).sendKeys('Test');
+        element(by.id('year')).sendKeys('Test');
         element(by.id('graduationDate')).sendKeys('Test');
         element(by.id('signup')).click();
 
@@ -65,9 +82,8 @@ describe('CEN3031 login', function() {
         signout();
     });
 
-    it('should sign up current member', function() {
-        newUserName = makeid();
-
+    it('should sign up prospective member', function() {
+        browser.get('http://localhost:3000');
         element(by.id('signup')).click();
 
         browser.driver.wait(function () {
@@ -78,13 +94,31 @@ describe('CEN3031 login', function() {
 
         element(by.id('firstName')).sendKeys('Test');
         element(by.id('lastName')).sendKeys('Test');
+        element(by.id('nickName')).sendKeys('Test');
         element(by.id('email')).sendKeys('test@test.com');
+        element(by.id('gatorlink')).sendKeys('test@ufl.edu');
         element(by.id('buttonCurrent')).click();
+        //element(by.id('instrument')).sendKeys('Trombone');
+        element(by.id('primary')).click();
+        element.all(by.repeater('instruments in instruments')).click();
+        element(by.id('primaryYears')).sendKeys('Test');
+        element(by.id('phoneNumber')).sendKeys('Test');
+        element(by.id('highSchool')).sendKeys('Test');
         element(by.id('username')).sendKeys(newUserNameCurrent);
         element(by.id('password')).sendKeys('TestTest');
-        element(by.id('instrument')).sendKeys('Trombone');
-        element(by.id('phoneNumber')).sendKeys('Test');
-        element(by.id('permanentAddress')).sendKeys('Test');
+        element(by.id('permanentLine1')).sendKeys('Test');
+        element(by.id('permanentLine2')).sendKeys('Test');
+        element(by.id('permanentCity')).sendKeys('Test');
+        element(by.id('permanentState')).sendKeys('Fl');
+        element(by.id('permanentZip')).sendKeys('32611');
+        element(by.id('localLine1')).sendKeys('Test');
+        element(by.id('localLine2')).sendKeys('Test');
+        element(by.id('localCity')).sendKeys('Test');
+        element(by.id('localState')).sendKeys('Fl');
+        element(by.id('localZip')).sendKeys('32611');
+        element(by.id('major')).sendKeys('Test');
+        element(by.id('class')).sendKeys('Test');
+        element(by.id('year')).sendKeys('Test');
         element(by.id('graduationDate')).sendKeys('Test');
         element(by.id('signup')).click();
 
@@ -98,9 +132,8 @@ describe('CEN3031 login', function() {
         signout();
     });
 
-    it('should sign up alumni member', function() {
-         newUserName = makeid();
-
+    it('should sign up prospective member', function() {
+        browser.get('http://localhost:3000');
         element(by.id('signup')).click();
 
         browser.driver.wait(function () {
@@ -111,13 +144,31 @@ describe('CEN3031 login', function() {
 
         element(by.id('firstName')).sendKeys('Test');
         element(by.id('lastName')).sendKeys('Test');
+        element(by.id('nickName')).sendKeys('Test');
         element(by.id('email')).sendKeys('test@test.com');
+        element(by.id('gatorlink')).sendKeys('test@ufl.edu');
         element(by.id('buttonAlumni')).click();
+        //element(by.id('instrument')).sendKeys('Trombone');
+        element(by.id('primary')).click();
+        element.all(by.repeater('instruments in instruments')).click();
+        element(by.id('primaryYears')).sendKeys('Test');
+        element(by.id('phoneNumber')).sendKeys('Test');
+        element(by.id('highSchool')).sendKeys('Test');
         element(by.id('username')).sendKeys(newUserNameAlumni);
         element(by.id('password')).sendKeys('TestTest');
-        element(by.id('instrument')).sendKeys('Trombone');
-        element(by.id('phoneNumber')).sendKeys('Test');
-        element(by.id('permanentAddress')).sendKeys('Test');
+        element(by.id('permanentLine1')).sendKeys('Test');
+        element(by.id('permanentLine2')).sendKeys('Test');
+        element(by.id('permanentCity')).sendKeys('Test');
+        element(by.id('permanentState')).sendKeys('Fl');
+        element(by.id('permanentZip')).sendKeys('32611');
+        element(by.id('localLine1')).sendKeys('Test');
+        element(by.id('localLine2')).sendKeys('Test');
+        element(by.id('localCity')).sendKeys('Test');
+        element(by.id('localState')).sendKeys('Fl');
+        element(by.id('localZip')).sendKeys('32611');
+        element(by.id('major')).sendKeys('Test');
+        element(by.id('class')).sendKeys('Test');
+        element(by.id('year')).sendKeys('Test');
         element(by.id('graduationDate')).sendKeys('Test');
         element(by.id('signup')).click();
 
