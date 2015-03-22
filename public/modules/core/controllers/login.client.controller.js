a56f480b36ddf7a812727fe48299f1d3b1c58473
JChauster <chaudown5@hotmail.com>
2015-03-22 19:14:06 -0400

    tried login path change (problem w/ signout)

--- public/modules/core/controllers/login.client.controller.js
+++ public/modules/core/controllers/login.client.controller.js
@@ -1,27 +1,17 @@
 'use strict';
-angular.module('users').controller('LoginController', ['$scope', '$http', '$location', 'Authentication',
-	function($scope, $http, $location, Authentication) {
-            $scope.authentication = Authentication;
-
-            $scope.instruments = ['Piccolo', 'Flute', 'Clarinet', 'Oboe', 'English Horn', 'Basson', 'Contrabass',
-            'Contrabass Bassoon', 'Alto Saxophone', 'Tenor Saxophone','Baritone Saxophone', 
-            'French Horn', 'Horn', 'Mellophone','Trumpet', 'Cornet', 'Baritone', 'Euphonium', 'Trombone',
-            'Bass Trombone', 'Tuba', 'Sousaphone', 'String Bass', 'Bass', 'Bass Guitar', 'Guitar',
-            'Percussion', 'Bass Drum', 'Quads', 'Tenors', 'Snare', 'Cymbals', 'Triangle', 'Timpani', 
-            'Marimba', 'Vibraphone', 'Xylophone', 'Glockenspiel', 'Drumset', 
-            'Celeste', 'Celesta', 'Piano', 'Harp', 'Bongos'];
-
-            $scope.instruments.sort();
-                
-            // If user is signed in then redirect back home
-            $scope.user = Authentication.user;
-            if($scope.user.userType==='Current')
-                $location.path('/home/current');
-            else if($scope.user.userType==='Prospective')
-                $location.path('/home/prospective');
-            else if($scope.user.userType==='Alumni')
-                $location.path('/home/alumni');
-            else
-                $location.path('/home');
+angular.module('users').controller('LoginController', ['$scope', '$location', 'Authentication',
+	function($scope, $location, Authentication) {
+            
+            //problems w/ signout
+//            $scope.authentication = Authentication;
+//            $scope.user = Authentication.user;
+//            if($scope.user.userType==='Current')
+//                $location.path('/home/current');
+//            else if($scope.user.userType==='Prospective')
+//                $location.path('/home/prospective');
+//            else if($scope.user.userType==='Alumni')
+//                $location.path('/home/alumni');
+//            else
+//                $location.path('http://localhost:3000/#!/');  
 	}
 ]);
