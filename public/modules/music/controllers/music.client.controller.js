5f37c38fc9d3b2b8af21396fcf17157b14a9fdf7
Derek Hua <dhua20@yahoo.com>
2015-04-05 18:56:24 -0400

    admin can now go to view music page

--- public/modules/music/controllers/music.client.controller.js
+++ public/modules/music/controllers/music.client.controller.js
@@ -5,11 +5,17 @@ angular.module('music').controller('MusicController', ['$scope', '$stateParams',
 	function($scope, $stateParams, $location, Authentication, Music) {
 		$scope.authentication = Authentication;
                
-                $scope.user = Authentication.user;
-                if($scope.user.roles !== 'admin')
-                    $location.path('/music');
-                
+               // allows for the user to be able to access the music view page
+               $scope.viewShow = "false";
                
+                $scope.user = Authentication.user;
+                if($scope.user.roles !== 'admin' || $scope.user.roles !== 'moderator') {
+                    $location.path('/music');               
+                } else {
+                    $scope.viewShow = "true";
+                }
+                
+                
                 $scope.instruments = ['Piccolo', 'Flute', 'Clarinet', 'Oboe', 'English Horn', 'Basson', 'Contrabass',
 		'Contrabass Bassoon', 'Alto Saxophone', 'Tenor Saxophone','Baritone Saxophone', 
 		'French Horn', 'Horn', 'Mellophone','Trumpet', 'Cornet', 'Baritone', 'Euphonium', 'Trombone',
