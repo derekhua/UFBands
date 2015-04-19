6c608751c7d6a85a743676f644ecc439136dc8a9
Gio De La Torre <g.delatorre@aol.com>
2015-04-19 14:30:20 -0400

    MOAR TESTING
    Testing completed for user model

--- app/tests/uniform.server.model.test.js
+++ app/tests/uniform.server.model.test.js
@@ -6,6 +6,7 @@
 var should = require('should'),
 	mongoose = require('mongoose'),
 	User = mongoose.model('User');
+	Uniform	=	mongoose.model('Uniform');
 
 /**
  * Globals
--- app/tests/user.server.model.test.js
+++ app/tests/user.server.model.test.js
@@ -20,23 +20,23 @@ describe('User Model Unit Tests:', function() {
 		user = new User({
 			firstName: 'Full',
 			lastName: 'Name',
-			displayName: 'Full Name',
-            userType: 'Current',
-			email: 'test@test.com',
+			phoneNumber: '5613862573',
+			gatorlink: 'test@ufl.edu',
+			email: 'test@ufl.edu',
+			primary: 'Baritone',
+			permanentAddress: ['test', '', 'test', 'FL', '32601'],
+			localAddress: ['test', '', 'test', 'FL', '32601'],
+			highSchool: 'test',
+			graduationDate: '2011',
+			class: '3EG',
+			major: 'Band',
+			year: 'freshman',
+      userType: 'Current',
 			username: 'username',
 			password: 'password',
 			provider: 'local'
 		});
-		user2 = new User({
-			firstName: 'Full',
-			lastName: 'Name',
-			displayName: 'Full Name',
-                        userType: 'Current',
-			email: 'test@test.com',
-			username: 'username',
-			password: 'password',
-			provider: 'local'
-		});
+
 
 		done();
 	});
@@ -50,12 +50,14 @@ describe('User Model Unit Tests:', function() {
 		});
 
 		it('should be able to save without problems', function(done) {
-			user.save(done);
+			return user.save(function(err) {
+				should.not.exist(err);
+				done();
+			});
 		});
 
 		it('should fail to save an existing user again', function(done) {
-			user.save();
-			return user2.save(function(err) {
+			return user.save(function(err) {
 				should.exist(err);
 				done();
 			});
@@ -68,7 +70,123 @@ describe('User Model Unit Tests:', function() {
 				done();
 			});
 		});
+
+		it('should be able to show an error when try to save without last name', function(done) {
+			user.lastName = '';
+			return user.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without first name', function(done) {
+			user.firstName = '';
+			return user.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without phone number', function(done) {
+			user.phoneNumber = '';
+			return user.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without gatorlink', function(done) {
+			user.gatorlink = '';
+			return user.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without email', function(done) {
+			user.email = '';
+			return user.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without primary instrument', function(done) {
+			user.primary = '';
+			return user.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without high school', function(done) {
+			user.highSchool = '';
+			return user.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without graduation data', function(done) {
+			user.graduationDate = '';
+			return user.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without class', function(done) {
+			user.class = '';
+			return user.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without major', function(done) {
+			user.major = '';
+			return user.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without year', function(done) {
+			user.year = '';
+			return user.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without user type', function(done) {
+			user.userType = '';
+			return user.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without username', function(done) {
+			user.username = '';
+			return user.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should be able to show an error when try to save without password', function(done) {
+			user.password = '';
+			return user.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+
+
 	});
+
+
 
 	after(function(done) {
 		User.remove().exec();
