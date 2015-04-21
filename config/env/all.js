d1f958c80aa7d10f0dcdae4f34432f6920a0b38f
jay-swa <jason.aaron.ortiz@gmail.com>
2015-04-21 13:05:22 -0400

    config stuff

--- config/env/all.js
+++ config/env/all.js
@@ -26,10 +26,7 @@ module.exports = {
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-				'app/node_modules/quagga/dist/quagga.js',
-				'public/lib/moment/moment.js',
-				'public/lib/moment/locale/*.js',
-				'public/lib/moment/min/moment-with-locales.js'
+				'app/node_modules/quagga/dist/quagga.js'
 			]
 		},
 		css: [
